f52a9d5c2cb9ae927e6e830236cd48b812026dd6
piresthiago10 <thiagopires.as@gmail.com>
2021-03-12 16:48:48 -0300

    hotfix - removidos

--- cms/calendario_aniversario/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Aniversario(models.Model):
-    nome = models.CharField(max_length=30)
-    data_aniversario = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.nome
