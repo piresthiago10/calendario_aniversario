c52677a9925f5e7ad0fa6b1f3090210ae133065d
piresthiago10 <thiagopires.as@gmail.com>
2021-03-15 13:14:06 -0300

    feature - adicionado dockstring

--- django/aniversarios/validators.py
+++ django/aniversarios/validators.py
@@ -1,6 +1,10 @@
 from datetime import date
 
 def data_aniversario_valido(data_aniversario):
+    """ Valida data de aniversário. 
+    Se a data de aniversário for maior do que
+    o dia de hoje o validador não permite
+    a criacão do resgistro """
 
     hoje = date.today()
 
@@ -10,4 +14,7 @@ def data_aniversario_valido(data_aniversario):
         return False
 
 def nome_valido(nome):
+    """ Valida nome do aniversariante.
+    Se a string possuir números o validador
+    não permite a ciração do registro. """
     return nome.isalpha()
